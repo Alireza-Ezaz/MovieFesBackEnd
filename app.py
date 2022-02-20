99fc54569782f2581046e52b1562b4d0eeb9bfcd
S.Alireza Ezaz <sar.ezaz@yahoo.com>
2022-02-20 11:53:06 +0330

    Created tables.

--- app.py
+++ app.py
@@ -1,8 +1,55 @@
-import flask
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, request, jsonify, Response, make_response
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+from flask_restful import Resource, Api
+from sqlalchemy.orm import backref
+from datetime import datetime
+from functools import wraps
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+api = Api(app)
+app.config['JSON_SORT_KEYS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MovieFes.db'  # Specify database name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    director = db.Column(db.String)
+    poster = db.Column(db.String)
+
+    def __init__(self, name, director, poster):
+        self.name = name
+        self.director = director
+        self.poster = poster
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+
+    def __init__(self, username):
+        self.username = username
+
+
+class Comment(db.Model):
+    __tablename__ = 'Comment'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String, nullable=False)
+    userId = db.Column(db.Integer, db.ForeignKey('User.id'))
+    user = db.relationship("User", backref=backref("User", uselist=False))
+    movieId = db.Column(db.Integer, db.ForeignKey('Movie.id'), nullable=False)
+    movie = db.relationship("Movie", backref=backref("Movie", uselist=False))
+
+    def __init__(self, userId, movieId, comment_body):
+        self.userId = userId
+        self.movieId = movieId
+        self.comment = comment_body
